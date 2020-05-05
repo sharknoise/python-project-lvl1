d4f7df6de7d9e9eba1bf4e8787ac4124fcc2aaad
sharkvoice <sharknoise@gmail.com>
2020-05-06 00:38:12 +0300

    add empty lines and spaces in accordance with flake8

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
 
+
 def main():
     """Print a greeting."""
     print("Welcome to the Brain Games!")
 
-#check if the script runs as a program
+
+# check if the script runs as a program
 if __name__ == "__main__":
     main()
