20a6bde598a92c889d5edf0b4662817d654d0661
sharkvoice <sharknoise@gmail.com>
2020-05-05 21:30:46 +0300

    create package structure as described in step 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# python-project-lvl1
+First project in the Hexlet Python Developer profession by student Dmitry Klimenko.
---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+def main():
+    """Print a greeting."""
+    print("Welcome to the Brain Games!")
+
+#check if the script runs as a program
+if __name__ == "__main__":
+    main()
